72a7f0460df30ef3828fe40c93295d358ee2df1e
zoryon <gioelespata@gmail.com>
2025-07-22 22:58:40 +0200

    sum

--- src/components/global/Nav/Bottombar.tsx
+++ src/components/global/Nav/Bottombar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Bottombar = () => {
     return (
-        <div className="w-screen h-[90px] flex justify-center items-center gap-5 bg-[rgb(40,40,40,0.9)] mt-10 uppercase text-background">
+        <div className="w-screen h-[90px] flex justify-center items-center gap-5 bg-[rgb(40,40,40,0.9)] mt-10 uppercase text-background text-xs lg:text-base">
             <Link href="https://www.emmeservicesrl.org" className="underline font-bold">
                 Privacy policy
             </Link>
